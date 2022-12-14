1b1755fa6c6b2178514675dec5f3612447a89c01
Ekelojuoti <ekelojuotisulaimon@gmail.com>
2022-12-13 16:24:30 -0800

    Task 12

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,14 +1,12 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - check the code for Holberton School students.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0.
  */
-
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	print_times_table(2);
+	return (0);
 }
