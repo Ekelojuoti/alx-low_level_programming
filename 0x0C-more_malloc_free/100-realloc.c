893d43c3a8db9ee7be495e7a17835d1f2d32b872
Ekelojuoti <ekelojuotisulaimon@gmail.com>
2023-01-12 14:49:15 -0800

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,17 +1,5 @@
 #include <stdlib.h>
 #include "main.h"
-/**
-  *array_range - creates an array of integers.
-  *@min: minimum value.
-  *@max: maximum value.
-  *
-  *Return: pointer to newly created array.
-  *NULL if malloc fails.
-  *NULL if min > max.
-  */
-int *array_range(int min, int max)
-#include <stdlib.h>
-#include "main.h"
 
 /**
  * *_realloc - reallocates a memory block using malloc and free
@@ -59,26 +47,4 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	free(ptr);
 	return (ptr1);
-}
-	int range, i;
-	int *p;
-
-	range = 0;
-
-	if (min > max)
-		return (NULL);
-
-	range = ((max + 1) - min);
-
-	p = malloc(range * sizeof(int));
-
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0; i < range; i++)
-	{
-		*(p + i) = min + i;
-	}
-
-	return (p);
 }
